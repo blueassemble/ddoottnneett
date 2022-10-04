a3a6080f04516e2bf444e773478e347d4db93fac
blueassemble <blueassemble@github.com>
2022-10-05 00:10:58 +0900

    literal & var

---
+++ literal-and-var/Program.cs
@@ -0,0 +1,7 @@
+﻿//declare name, message counts, and temperature
+string name = "Bob";
+int messageCount = 3;
+double temperature = 34.4;
+
+//print "Hello Bob, you have 3 messages.The temperature is 34.4"
+Console.WriteLine("Hello, " + name + "! you have " + messageCount + " messages in your box.The temperature is " + temperature + "celcius.");
