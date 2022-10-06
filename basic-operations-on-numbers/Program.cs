98cc7fcd34e1a262ec842ad40feae9b50899bfa0
blueassemble <blueassemble@github.com>
2022-10-07 01:24:12 +0900

    basic operations on nubers in c#

---
+++ basic-operations-on-numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * 5 / 9;
+
+//wirteline celsius
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
+//get user input for farhenheit one line
+Console.Write("Enter a temperature in Fahrenheit: " );
+fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+//convert fahrenheit to celsius
+celsius = (fahrenheit - 32) * 5 / 9;
+
+//wirteline celsius
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
