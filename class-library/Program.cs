f6001a3ec1240b701fe7397d75dfd9b5b14c5797
blueassemble <blueassemble@github.com>
2022-10-08 00:43:33 +0900

    Class and Library

---
+++ class-library/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+//compare firstValue and secondValue with System.Math class
+largerValue = Math.Max(firstValue, secondValue);
+
+
+Console.WriteLine("The larger value is {0}", largerValue);
+
