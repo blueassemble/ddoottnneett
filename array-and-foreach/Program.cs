78b2802cc048280f7a47820773c53f88f6a10067
blueassemble <rncjdghl@gmail.com>
2022-10-12 23:54:54 +0900

    Array and For Each

---
+++ array-and-foreach/Program.cs
@@ -0,0 +1,13 @@
+﻿//declare array and initialize it
+string[] names = new string[8] { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+//Iterate through each element of the array
+foreach (string name in names)
+{
+    //check if the element starts with B
+    if (name.StartsWith("B"))
+    {
+        //print the element
+        Console.WriteLine(name);
+    }
+}
